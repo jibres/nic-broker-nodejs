ad8e26c0f5240ed12c82b76b868b14c0d68b1c67
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-13 03:15:33 +0430

    add .env lib to read secret_key

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// load .env file to get secret_key
+require('dotenv').config();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Jibres Nic Broker' });
   res.json({test: "MrAdib"});
+  console.log(process.env.SECRET_KEY)
 });
 
 router.post('/', function(req, res, next) {
