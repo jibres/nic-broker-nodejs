537c671c6ad577d2e4450dd527ee37944425295f
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-13 03:05:41 +0430

    show sample json result

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jibres Nic Broker' });
+  // res.render('index', { title: 'Jibres Nic Broker' });
+  res.json({test: "MrAdib"});
+});
+
+router.post('/', function(req, res, next) {
+  res.json({test: "MrAdib"});
 });
 
 module.exports = router;
