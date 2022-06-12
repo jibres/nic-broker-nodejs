046f6d7d7435eb4957e73f0d51d160291889c24f
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-13 03:44:04 +0430

    check pem file exist

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,33 @@ var router = express.Router();
 // load .env file to get secret_key
 require('dotenv').config();
 
+const fs = require('fs');
+const pathPemFile = 'secret/nic.pem'
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Jibres Nic Broker' });
   
-  if(!process.env.SECRET_KEY)
+  if(process.env.SECRET_KEY)
+  {
+    try {
+      const pemFile = fs.readFileSync(pathPemFile, 'utf8')
+      // console.log(pemFile);
+      res.json({test: "MrAdib"});
+    } catch (err) {
+      // console.error(err);
+      res.status(501).send("Pem file not eixst! Please complete installation...").end();
+    }
+  }
+  else
   {
     res.status(501).send("Secret key is not defined! Please complete installation...").end();
   }
-
-  // res.json({test: "MrAdib"});
-  // console.log(process.env.SECRET_KEY)
 });
 
 router.post('/', function(req, res, next) {
-  
+  // res.json({test: "MrAdib"});
 });
 
 module.exports = router;
