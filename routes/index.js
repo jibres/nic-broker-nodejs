b227e7436f7a4c47c3dc98b7fa5ab5c5e49969ef
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-13 03:48:43 +0430

    add some constants

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var router = express.Router();
 require('dotenv').config();
 
 const fs = require('fs');
-const pathPemFile = 'secret/nic.pem'
-
+const curlSecret = 'secret/nic.pem'
+const apiURL = 'https://epp.nic.ir/submit';
+const curlAgent = 'Mozilla/5.0 (compatible; Jibres irnic/2.0; +https://jibres.com/bot)';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,9 +16,14 @@ router.get('/', function(req, res, next) {
   if(process.env.SECRET_KEY)
   {
     try {
-      const pemFile = fs.readFileSync(pathPemFile, 'utf8')
+      const pemFile = fs.readFileSync(curlSecret, 'utf8')
       // console.log(pemFile);
       res.json({test: "MrAdib"});
+      // pem file exist and 
+
+      
+
+
     } catch (err) {
       // console.error(err);
       res.status(501).send("Pem file not eixst! Please complete installation...").end();
