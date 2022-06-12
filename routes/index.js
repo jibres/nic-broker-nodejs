93a6df400dfc49281362c68c249c64622d3d2831
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-13 03:27:07 +0430

    if config file does not exist show 501 error

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,18 @@ require('dotenv').config();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Jibres Nic Broker' });
-  res.json({test: "MrAdib"});
-  console.log(process.env.SECRET_KEY)
+  
+  if(!process.env.SECRET_KEY)
+  {
+    res.status(501).send("Secret key is not defined! Please complete installation...").end();
+  }
+
+  // res.json({test: "MrAdib"});
+  // console.log(process.env.SECRET_KEY)
 });
 
 router.post('/', function(req, res, next) {
-  res.json({test: "MrAdib"});
+  
 });
 
 module.exports = router;
