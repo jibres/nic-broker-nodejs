e5159a091e09df078d33545ac8c503a328f18891
MrAdib <Mr.Javad.Adib@gmail.com>
2022-06-14 00:32:02 +0430

    check token and if correct continue

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ const curlAgent = 'Mozilla/5.0 (compatible; Jibres irnic/2.0; +https://jibres.co
 
 // prepare request to irnic server
 const https = require('https');
+const url = require('url');
 const axios = require('axios');
 
 /* GET home page. */
@@ -19,31 +20,39 @@ router.get('/', function(req, res, next) {
   
   if(process.env.SECRET_KEY)
   {
-    try {
-      const pemFile = fs.readFileSync(curlSecret, 'utf8')
-
-      const httpsAgent = new https.Agent({
-        ca: pemFile
-      });
-
-      // send request to irnic
-      const response = axios.get(apiURL, { httpsAgent });
-
-      axios.post(apiURL, { httpsAgent })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-
-      // sample output
-      res.json({test: "MrAdib"});
-
-    } catch (err) {
-      // console.error(err);
-      res.status(501).send("Pem file not eixst! Please complete installation...").end();
+    const queryObject = url.parse(req.url, true).query;
+    if(queryObject.token && queryObject.token === process.env.SECRET_KEY)
+    {
+      try {
+        const pemFile = fs.readFileSync(curlSecret, 'utf8')
+  
+        const httpsAgent = new https.Agent({
+          ca: pemFile
+        });
+  
+        // send request to irnic
+        const response = axios.get(apiURL, { httpsAgent });
+  
+        // axios.post(apiURL, { httpsAgent })
+        // .then(function (response) {
+        //   console.log(response);
+        // })
+        // .catch(function (error) {
+        //   console.log(error);
+        // });
+  
+  
+        // sample output
+        res.json({test: "MrAdib"});
+  
+      } catch (err) {
+        // console.error(err);
+        res.status(501).send("Pem file not eixst! Please complete installation...").end();
+      }
+    }
+    else
+    {
+      res.status(403).send("Secret key is incorrect!").end();
     }
   }
   else
